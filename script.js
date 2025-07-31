65e21b3bc84ea3a7cc40b04250afe2bf16e63756
anthonyduarteaa <anthony.duarte.barbosa@escola.pr.gov.br>
2025-07-31 08:46:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function calcula(){
+    let C = parseFloat(document.getElementById('C').value);
+    let P = parseFloat(document.getElementById('P').value);
+    let T = parseFloat(document.getElementById('T').value);
+
+    if(C == 0){
+        alert("O capital não pode ser 0!");
+        return;
+    }
+
+    if(P == 0){
+        alert("A porcentagem não pode ser 0!");
+        return;
+    }
+
+    if(T == 0){
+        alert("O tempo não pode ser 0!");
+        return;
+    }
+
+    let newP = P/ (100);
+    let J = C * newP * T;
+    let M = C + J; 
+    
+    document.getElementById('resultado1').textContent = "O valor dos juros é " + J;
+
+    document.getElementById('resultado').textContent = "O montante final é " + M;
+
+}
